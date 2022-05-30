693e47c65916ac1d5ca7d6e5438a49ccb9178233
MuskanVishwakarmaa <105989631+MuskanVishwakarmaa@users.noreply.github.com>
2022-05-30 13:37:23 +0530

    program to multiply all values in the list

---
+++ numpy/num2_product.py
@@ -0,0 +1,8 @@
+#Q2. Write a Python program to multiply all values in the list using numpy.prod().
+import numpy 
+lst1 = [1, 2, 3] 
+lst2 = [4, 5, 6]
+result1 = numpy.prod(lst1)
+result2 = numpy.prod(lst2)
+print("List 1=",result1)
+print("List 2=",result2)
