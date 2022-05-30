7612be1e7a45967669791c7c54a68a6c3805c265
MuskanVishwakarmaa <105989631+MuskanVishwakarmaa@users.noreply.github.com>
2022-05-30 16:41:46 +0530

    if it is not a number generates an error message

---
+++ Miscellaneous/exception handling/exh1_alpha.py
@@ -0,0 +1,10 @@
+#Q1. Write a Python program to input a number, if it is not a number generates an error message.
+
+while True:
+    try:
+        a = int(input("Input a number: "))
+        break
+    except ValueError:
+        print("\nThis is not a number. Try again...")
+        print()
+		
